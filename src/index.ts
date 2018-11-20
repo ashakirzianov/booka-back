cb51d03093e7df0adc2b15bfbcd1332151e3794f
ashakirzianov <ashakirzianov@icloud.com>
2018-11-20 17:15:10 +0400

    Serve 'Hello world' on get

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,8 @@ import * as route from 'koa-route';
 const port = process.env.PORT || 3042;
 const app = new Koa();
 
+app.use(route.get('/', ctx => {
+    ctx.body = 'Hello world';
+}));
+
 app.listen(port);
