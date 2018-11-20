31cd777177b02351f0b819f6b03710193b43f227
ashakirzianov <ashakirzianov@icloud.com>
2018-11-20 19:28:25 +0400

    Serve War&Peace on any get

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import * as Koa from 'koa';
 import * as route from 'koa-route';
+import * as send from 'koa-send';
 
 const port = process.env.PORT || 3042;
 const app = new Koa();
 
-app.use(route.get('/', ctx => {
-    ctx.body = 'Hello world';
+app.use(route.get('/*', async ctx => {
+    await send(ctx, 'public/wap.epub');
 }));
 
 app.listen(port);
