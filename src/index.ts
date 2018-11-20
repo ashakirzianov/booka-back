fc7d3ac3798b03fa717d6b9b37ff9ba767c8c496
ashakirzianov <ashakirzianov@icloud.com>
2018-11-20 19:46:43 +0400

    Get file name from route

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import * as send from 'koa-send';
 const port = process.env.PORT || 3042;
 const app = new Koa();
 
-app.use(route.get('/*', async ctx => {
-    ctx.set('Content-Disposition', 'attachment; filename="wap.epub"');
-    await send(ctx, 'public/wap.epub');
+app.use(route.get('/:fileName', async (ctx, fileName) => {
+    const fullName = fileName + '.epub';
+    ctx.set('Content-Disposition', `attachment; filename="${fullName}"`);
+    await send(ctx, `public/${fullName}`);
 }));
 
 app.listen(port);
