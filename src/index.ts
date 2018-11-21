7c68687ec38db3872229e130a36e2f812a9ece46
ashakirzianov <ashakirzianov@icloud.com>
2018-11-21 13:57:41 +0400

    Using cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as Koa from 'koa';
 import * as route from 'koa-route';
 import * as send from 'koa-send';
+import * as cors from 'koa-cors';
 
 const port = process.env.PORT || 3042;
 const app = new Koa();
+
+app.use(cors());
 
 app.use(route.get('/epub/:fileName', async (ctx, fileName) => {
     const fullName = fileName + '.epub';
