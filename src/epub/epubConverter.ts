ee4212dcaf8d8c39dc5635547f11926886fecbef
Anton Shakirzianov <ashakirzianov@icloud.com>
2019-04-18 17:40:06 -0300

    Remove 'EpubConverter' type

--- src/epub/epubConverter.ts
+++ src/epub/epubConverter.ts
@@ -1,33 +1,11 @@
 import * as C from '../contracts';
 import { ParsedEpub } from './epubParser';
-import { converter as traumConverter } from './traumConverter';
+import { convertEpub } from './traumConverter';
 import { epub2Parser } from './epub2';
-
-export type EpubConverter = {
-    canHandleEpub: (epub: ParsedEpub) => boolean,
-    convertEpub: (epub: ParsedEpub) => Promise<C.BookContent>,
-};
-
-const convertersRegistry = [
-    traumConverter,
-];
 
 export async function path2book(path: string): Promise<C.BookContent> {
     const epub = await epub2Parser.parseFile(path);
-    const book = epub2book(epub);
+    const book = convertEpub(epub);
 
     return book;
-}
-
-function epub2book(epub: ParsedEpub): Promise<C.BookContent> {
-    const converter = resolveEpubConverter(epub);
-    const book = converter(epub);
-
-    return book;
-}
-
-function resolveEpubConverter(epub: ParsedEpub): (epub: ParsedEpub) => Promise<C.BookContent> {
-    const converter = convertersRegistry.find(c => c.canHandleEpub(epub)) || traumConverter;
-
-    return converter.convertEpub;
 }
--- src/epub/traumConverter.ts
+++ src/epub/traumConverter.ts
@@ -1,17 +1,139 @@
-import { BookContent } from '../contracts';
-import { EpubConverter } from './epubConverter';
-import { ParsedEpub } from './epubParser';
-import { buildBook } from './traumConverter.book';
+import {
+    translate, headParser, Parser,
+    choice, seq, some, messageToString,
+    XmlNodeWithChildren,
+} from '../xml';
+import { section } from './traumConverter.section';
+import { ParsedEpub, EpubSection, EpubCollection } from './epubParser';
+import { filterUndefined, toArray } from '../utils';
+import { BookContent, BookNode, ChapterNode, ParagraphNode, Footnote } from '../contracts';
+import { logString } from '../logger';
 
-export const converter: EpubConverter = {
-    canHandleEpub: canHandleEpub,
-    convertEpub: convertEpub,
+type HeaderElement = {
+    element: 'header',
+    title: string,
+    level: number,
 };
 
-function canHandleEpub(epub: ParsedEpub): boolean {
-    return true;
+type ParagraphElement = {
+    element: 'paragraph',
+    paragraph: ParagraphNode,
+};
+
+type TitleElement = {
+    element: 'title',
+    author?: string,
+    title?: string,
+};
+
+type FootnoteElement = {
+    element: 'footnote',
+    footnote: Footnote,
+};
+
+type IgnoreElement = {
+    element: 'ignore',
+};
+
+type Element =
+    | HeaderElement
+    | ParagraphElement
+    | TitleElement
+    | FootnoteElement
+    | IgnoreElement
+    ;
+
+export async function convertEpub(epub: ParsedEpub): Promise<BookContent> {
+    const elements = await toArray(sections2elements(epub.sections()));
+
+    const title = buildTitle(elements);
+    const content = buildContent(elements);
+    const footnotes = buildFootnotes(elements);
+
+    // TODO: report when no title page
+    return {
+        meta: {
+
+            title: title && title.title || epub.metadata.title,
+            author: title && title.author || epub.metadata.author,
+        },
+        nodes: content,
+        footnotes: footnotes,
+    };
 }
 
-function convertEpub(epub: ParsedEpub): Promise<BookContent> {
-    return buildBook(epub);
+async function* sections2elements(secs: EpubCollection<EpubSection>) {
+    for await (const sec of secs) {
+        const els = tree2elements(sec.content);
+        yield* els;
+    }
+}
+
+function tree2elements(tree: XmlNodeWithChildren): Element[] {
+    const result = section(tree.children);
+
+    // TODO: implement better logging strategy
+    const message = messageToString(result.message);
+    if (message) {
+        const prefix = result.success ? 'Warnings: ' : 'Errors: ';
+        logString(prefix + message);
+    }
+    return result.success ? result.value : [];
+}
+
+function buildTitle(structures: Element[]): TitleElement | undefined {
+    const titleResult = structures.find(s => s.element === 'title');
+    return titleResult as TitleElement;
+}
+
+const headElement = headParser<Element>();
+
+function chapterParser<T extends BookNode>(level: number, contentE: Parser<Element[], T>): Parser<Element[], BookNode> {
+    return choice(
+        translate(
+            seq(
+                headElement(se => se.element === 'header' && se.level === level ? se : null),
+                some(contentE),
+            ),
+            ([h, c]) => ({
+                node: 'chapter' as 'chapter',
+                level: level,
+                title: h.title,
+                nodes: c,
+            } as ChapterNode),
+        ),
+        contentE,
+    );
+}
+
+const paragraph = headElement(
+    se => se.element === 'paragraph'
+        ? se.paragraph
+        : null
+);
+
+const h6 = chapterParser(-2, paragraph);
+const h5 = chapterParser(-1, h6);
+const h4 = chapterParser(0, h5);
+const h3 = chapterParser(1, h4);
+const h2 = chapterParser(2, h3);
+const h1 = chapterParser(3, h2);
+
+const bookContent = h1;
+const skipOne = headElement(n => undefined);
+const book = translate(
+    some(choice(bookContent, skipOne)),
+    nodes => filterUndefined(nodes),
+);
+
+function buildContent(structures: Element[]): BookNode[] {
+    const result = book(structures);
+    return result.value;
+}
+
+function buildFootnotes(structures: Element[]): Footnote[] {
+    return structures
+        .filter((e): e is FootnoteElement => e.element === 'footnote')
+        .map(e => e.footnote)
+        ;
 }
