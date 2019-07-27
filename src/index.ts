54016ca51b3c296ca94d5b4ead131c7fdf76c69e
ashakirzianov <ashakirzianov@icloud.com>
2019-07-26 18:39:32 -0700

    Config env variables (dotenv)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as Koa from 'koa';
 import * as cors from '@koa/cors';
+import { config as configEnv } from 'dotenv';
 import { router } from './api';
 import { connectDb } from './connect';
 
+configEnv();
 const port = process.env.PORT || 3042;
 const app = new Koa();
 
