549d9c7ed7a9f27c3a42ff7e5a0674e38d9c2f3c
ashakirzianov <ashakirzianov@icloud.com>
2019-07-31 18:14:11 -0700

    Implement 'generateToken'

---
+++ src/auth/token.ts
@@ -0,0 +1,15 @@
+import * as jwt from 'jsonwebtoken';
+import { config } from './config';
+
+const jwtConfig = config().jwt;
+export function generateToken(userId: string) {
+    const token = jwt.sign({}, jwtConfig.secret, {
+        // TODO: extract as const ?
+        expiresIn: '1 month',
+        audience: jwtConfig.audience,
+        issuer: jwtConfig.issuer,
+        subject: userId,
+    });
+
+    return token;
+}
