210c6d2ffe703bed122df90fce0ca3931d6b3312
ashakirzianov <ashakirzianov@icloud.com>
2019-08-05 13:36:43 -0700

    Setup https

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as Koa from 'koa';
 import * as cors from '@koa/cors';
+import * as https from 'https';
+import * as fs from 'fs';
 import { config as configEnv } from 'dotenv';
 import { router } from './api';
 import { connectDb } from './connect';
 import { passport } from './auth';
+import { config } from './config';
 
 configEnv();
 startup(new Koa());
@@ -20,5 +23,22 @@ async function startup(app: Koa) {
         .use(router.allowedMethods());
 
     const port = process.env.PORT || 3042;
-    app.listen(port);
+    const options = serverOptions();
+    https
+        .createServer(options, app.callback())
+        .listen(port);
+}
+
+function serverOptions(): https.ServerOptions {
+    const sslConfig = config().ssl;
+    if (sslConfig) {
+        if (fs.existsSync(sslConfig.keyPath) && fs.existsSync(sslConfig.certPath)) {
+            return {
+                key: fs.readFileSync(sslConfig.keyPath),
+                cert: fs.readFileSync(sslConfig.certPath),
+            };
+        }
+    }
+
+    return {};
 }
