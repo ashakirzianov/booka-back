8923d1723b92277dfdd87cac17f32279f715c449
ashakirzianov <ashakirzianov@icloud.com>
2019-08-05 19:32:20 -0700

    Add to collection on book upload

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,6 +1,26 @@
 import * as KoaRouter from 'koa-router';
+import { logTimeAsync, logger } from '../log';
+import { insertBook } from '../db';
+import { loadEpubPath } from '../bookConverter';
 
 export const uploadRouter = new KoaRouter();
 
 uploadRouter.post('/upload', ctx => {
+    const files = ctx.request.files;
+    const book = files && files.book;
+    if (book) {
+        parseAndInsert(book.path);
+    }
 });
+
+async function parseAndInsert(fullPath: string) {
+    try {
+        const book = await logTimeAsync(
+            `Parse: ${fullPath}`,
+            () => loadEpubPath(fullPath)
+        );
+        await insertBook(book);
+    } catch (e) {
+        logger().warn(`While parsing '${fullPath}' error: ${e}`);
+    }
+}
