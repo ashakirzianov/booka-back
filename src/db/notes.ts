40470c234ef76b93c16ff378c626032d478fd349
ashakirzianov <ashakirzianov@icloud.com>
2019-08-25 16:24:59 +0700

    Implement 'get one note' db operation

--- src/db/notes.ts
+++ src/db/notes.ts
@@ -1,5 +1,5 @@
 import {
-    HasId,
+    HasId, Note, NoteContentNode,
 } from 'booka-common';
 import { model, DataFromModel, ObjectId } from '../back-utils';
 
@@ -22,5 +22,22 @@ const schema = {
 const docs = model('Note', schema);
 type DbNote = DataFromModel<typeof docs>;
 
+async function getOne(userId: string, noteId: string): Promise<Note & HasId | undefined> {
+    const doc = await docs.findById(noteId).exec();
+    if (doc && doc.userId === userId) {
+        const note: Note & HasId = {
+            _id: doc._id,
+            data: {
+                content: doc.content as NoteContentNode[],
+                title: doc.title,
+            },
+            lastEdited: doc.lastEdited,
+        };
+        return note;
+    }
+    return undefined;
+}
+
 export const notes = {
+    getOne,
 };
