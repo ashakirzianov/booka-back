8a793f96b16f9b0878034aa69a7c52c788b93bd3
ashakirzianov <ashakirzianov@icloud.com>
2019-08-30 14:26:42 +0700

    Implement 'get user history' operation

--- src/db/historyEvents.ts
+++ src/db/historyEvents.ts
@@ -1,5 +1,5 @@
-import { HistoryEventKind } from 'booka-common';
-import { model, DataFromModel, ObjectId } from '../back-utils';
+import { HistoryEventKind, HistoryEvent, HasId } from 'booka-common';
+import { model, DataFromModel, ObjectId, paginate } from '../back-utils';
 
 const schema = {
     userId: {
@@ -14,10 +14,29 @@ const schema = {
         type: String,
         required: true,
     },
+    date: {
+        type: Date,
+        required: true,
+    },
 } as const;
 
 const docs = model('BookHistory', schema);
 type DbHistoryEvent = DataFromModel<typeof docs>;
 
+async function forUser(userId: string, page: number): Promise<Array<HistoryEvent & HasId>> {
+    const result = await paginate(
+        docs.find({ userId }),
+        page,
+    ).exec();
+
+    return result.map(r => ({
+        _id: r._id,
+        kind: r.kind as HistoryEventKind,
+        date: r.date,
+        bookId: r.bookId,
+    }));
+}
+
 export const history = {
+    forUser,
 };
