d2af2fc3384adc6fd53a6b450e3b212ac1efd25b
ashakirzianov <ashakirzianov@icloud.com>
2020-02-21 16:13:47 -0300

    'CardCollection' db document

---
+++ src/db/collections.ts
@@ -0,0 +1,63 @@
+import { CardCollectionName } from 'booka-common';
+import { model, DataFromModel, ObjectId } from 'booka-utils';
+
+const schema = {
+    accountId: {
+        type: ObjectId,
+        required: true,
+    },
+    bookId: {
+        type: String,
+        required: true,
+    },
+    collectionName: {
+        type: String,
+        required: true,
+    },
+} as const;
+
+const docs = model('CardCollection', schema);
+type DbCollection = DataFromModel<typeof docs>;
+
+async function bookIds(accountId: string, collectionName: CardCollectionName): Promise<string[]> {
+    const result = await docs
+        .find({
+            accountId,
+            collectionName,
+        })
+        .select('bookId')
+        .exec();
+
+    return result.map(r => r.bookId);
+}
+
+async function add(accountId: string, bookId: string, collectionName: CardCollectionName): Promise<boolean> {
+    const doc: DbCollection = {
+        accountId,
+        bookId,
+        collectionName,
+    };
+
+    const result = await docs.update(
+        { accountId, bookId, collectionName },
+        { upsert: true },
+    ).exec();
+
+    return result ? true : false;
+}
+
+async function remove(accountId: string, bookId: string, collectionName: CardCollectionName): Promise<boolean> {
+    const result = await docs.deleteMany({
+        accountId,
+        bookId,
+        collectionName,
+    });
+
+    return true;
+}
+
+export const tags = {
+    bookIds,
+    add,
+    remove,
+};
